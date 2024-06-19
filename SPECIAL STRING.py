16efd6dbc6ad6996cd26fbf0ca3625d9278e4b30
Shaziyasheik <118586132+Shaziyasheik@users.noreply.github.com>
2024-06-19 09:22:30 +0530

    Add files via upload

---
+++ SPECIAL STRING.py
@@ -0,0 +1,13 @@
+#SPECIAL STRING
+a=input()
+s=input()
+total=0
+for i in a:
+    if i not in s:
+        temp=125
+        for j in s:
+            d=abs(ord(i)-ord(j))
+            if d<temp:
+                temp=d
+        total+=temp
+print(total)
